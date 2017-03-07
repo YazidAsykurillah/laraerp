851b3ef3b2776df5cd1bfae5f638b10e1179e6e0
iim <iim@prime-strategy.co.id>
2017-03-07 11:12:43 +0700

    report neraca hardcode

--- app/helper.php
+++ app/helper.php
@@ -3,12 +3,12 @@
     // My common functions
     function amount_sales_order_bank($reference,$chart_account_id)
     {
-        $bank_sales_invoice_payment = \DB::table('bank_sales_invoice_payment')->where('bank_id',$reference)->get();
-        $s_i_p = [];
-        foreach ($bank_sales_invoice_payment as $key) {
-            $s_i_p[] = \DB::table('sales_invoice_payments')->where('id',$key->sales_invoice_payment_id)->sum('amount');
-            print_r($s_i_p[]);
-        }
-
-        return $s_i_p;
+        // $bank_sales_invoice_payment = \DB::table('bank_sales_invoice_payment')->where('bank_id',$reference)->get();
+        // $s_i_p = [];
+        // foreach ($bank_sales_invoice_payment as $key) {
+        //     $s_i_p[] = \DB::table('sales_invoice_payments')->where('id',$key->sales_invoice_payment_id)->sum('amount');
+        //     print_r($s_i_p[]);
+        // }
+        //
+        // return $s_i_p;
     }
--- resources/views/pdf/neraca_montly.blade.php
+++ resources/views/pdf/neraca_montly.blade.php
@@ -22,9 +22,16 @@
         th{
             text-align: center;
         }
-        table tr .sub_child{
-            color: green;
+        table tr .parent{
+            color: navy;
             padding-left: 20px;
+        }
+        table tr .sub_child{
+            padding-left: 40px;
+        }
+        table tr .total_child{
+            color: navy;
+            padding-left: 40px;
         }
     </style>
 </head>
@@ -40,21 +47,32 @@
                     </div>
                     <br/>
                     <div class="box-body">
-                        <table class="table" style="width:100%">
+                        <table style="width:100%" border="0">
                             <tbody>
+                                <tr>
+                                    <td style="width:70%;color:navy">Harta</td>
+                                    <td style="width:30%;text-align:right;border-bottom:1px solid black">IDR</td>
+                                </tr>
                                 @foreach($chart_account as $key)
                                 <tr>
-                                    <td>{{ $key->description }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $key->account_number }}</td>
+                                    <td class="parent">{{ $key->description }}</td>
+                                    <td></td>
                                 </tr>
                                     @foreach($key->sub_chart_account as $sub_chart)
                                         <tr>
                                             @if($sub_chart->chart_account_id == 8)
                                             <td class="sub_child">{{ $sub_chart->banks->name }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $sub_chart->account_number }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{!! amount_sales_order_bank($sub_chart->reference,$sub_chart->chart_account_id) !!}</td>
+                                            <td align="right">50,000,000.00</td>
                                             @else
                                             <td class="sub_child">{{ $sub_chart->cashs->name }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ $sub_chart->account_number }}</td>
+                                            <td align="right">20,000,000.00</td>
                                             @endif
                                         </tr>
                                     @endforeach
+                                        <tr>
+                                            <td class="total_child">Total {{ $key->description }}</td>
+                                            <td style="border-top:1px solid black;color:navy" align="right">40,000,000.00</td>
+                                        </tr>
                                 @endforeach
 
                                 <!-- <tr>
